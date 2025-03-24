1afcf0383396278299d8d9726861c42ff8c0b3ff
NomanSarwar770 <nomansarwark@gmail.com>
2025-03-24 03:31:46 -0700

    updated

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ import 'primeicons/primeicons.css'
 import Tooltip from 'primevue/tooltip'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
+import Dialog from 'primevue/dialog'
 const app = createApp(App)
-
+app.component('Dialog', Dialog)
 app.use(router)
 app.directive('tooltip', Tooltip)
 app.use(PrimeVue, {
