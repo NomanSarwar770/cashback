fad27b125a117259dd5d618bff1426ec7cab2c22
Irfan khan <irfank824@gmail.com>
2025-04-11 21:11:27 +0500

    image fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,9 @@ import { ref, computed } from 'vue';
 import { RouterLink } from 'vue-router';
 import Button from 'primevue/button';
 import Dialog from 'primevue/dialog';
-import img1 from '@/assets/Slider 1.png'
-import img2 from '@/assets/Slider 3.png'
-import img3 from '@/assets/Slider 2.png'
+import img1 from '@/assets/slider 1.jpg'
+import img2 from '@/assets/slider 3.png'
+import img3 from '@/assets/slider 2.jpg'
 import img4 from '@/assets/athleta.jpg'
 import img5 from '@/assets/lenovo.png'
 
